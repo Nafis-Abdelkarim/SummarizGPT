6cdbb9dce08cb0e21cff358a3601c1b5057e04f2
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-06-20 23:29:48 +0100

    Start the demo section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,15 @@ const Hero = () => {
     return (
         <header className='w-full flex justify-center items-center flex-col'>
             <nav className='flex justify-between items-center w-full mb-10 pt-5'>
-                <img src={logo} alt="sumz_logo" className='w-28 object-contain' />
+                <div>
+                    <img src={logo} alt="sumz_logo" className='w-28 object-contain' />
+                </div>
                 <button type='button' 
                 onClick={() => window.open('https://github.com/Nafis-Abdelkarim/SummarizGPT')}
                 className='black_btn'>GitHub</button>
             </nav>
             <h1 className='head_text'>
-                summarize Articles with <br className='max-md:hidden'/>
+                Summarize Articles with <br className='max-md:hidden'/>
                 <span className='orange_gradient'>OpenAI GPT-4</span>
             </h1>
             <h2 className='desc'>
